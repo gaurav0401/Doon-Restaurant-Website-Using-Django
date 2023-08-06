6e1a629dec193193361383985e7d7a9720d43c76
gaurav_04_01 <gaurav.tnv6970@gmail.com>
2023-08-06 22:00:25 +0530

    deploy

--- project1/project1/settings.py
+++ project1/project1/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-(ld#)9r^630#_n179w8+-vp&t$ypr&^z84#7k*c!-aplcy-aqf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
